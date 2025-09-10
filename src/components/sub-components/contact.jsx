dd497a2616b1ca4539955b8b543a5780554fe90d
Gabriel Reis <55472396+gabrielsrs@users.noreply.github.com>
2025-09-10 06:21:59 -0300

    Updates

--- src/components/sub-components/contact.jsx
+++
@@ -1,8 +0,0 @@
-export function Contact({ icon: Icon, text }) {
-    return (
-        <div className="flex items-center space-x-2">
-            <Icon className="h-4 w-4" />
-            <span>{text}</span>
-        </div>
-    )
-}
--- src/components/sub-components/nav-link.jsx
+++
@@ -1,5 +0,0 @@
-export function NavLink({ navSelect, selected, content, scrollTo}) {
-    return (
-        <span className={`${navSelect == selected && "border-b-[1px] border-b-[#1E3A5F]"} hover:text-primary transition-colors text-center cursor-pointer`} onClick={() => scrollTo(selected)}>{content}</span>
-    )
-}
